521831f2f16ed48ac8a08d67aea086f5adad5de2
Vertmo <basile.pesin@gmail.com>
2018-05-30 11:54:23 +0200

    Ajouté les symboles sur la tombe

---
+++ Assets/Scripts/HiddenTexture.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenTexture : MonoBehaviour {
+	public GameObject light;
+	private Transform tfLight;
+
+	// Use this for initialization
+	void Start () {
+		if(light) tfLight = light.transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(tfLight) {
+			GetComponent<Renderer>().material.SetVector("_LightPos", tfLight.position);
+    	GetComponent<Renderer>().material.SetVector("_LightDir", tfLight.forward);
+		}
+	}
+}
