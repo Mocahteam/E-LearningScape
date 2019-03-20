d1d0e7ddabc55d6c6dc25e9d6270cde53f4a2069
garbarini <delia.garbari@gmail.com>
2019-03-20 16:53:35 +0100

    Toogle message console from settings main menu

---
+++ Assets/Scripts/AccessibilityButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccessibilityButton : MonoBehaviour {
+
+    public void SetAccessibilitySettings()
+    {
+        Debug.Log("Value is changing"); //Afficher ce message dans la console 
+
+    }
+}
