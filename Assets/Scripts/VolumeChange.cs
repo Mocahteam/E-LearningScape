9ebf088613337a63520b54aec2fbfbc304f49f4f
garbarini <delia.garbari@gmail.com>
2019-03-20 16:37:56 +0100

    Settings option Main menu appear or disappear

---
+++ Assets/Scripts/VolumeChange.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeChange : MonoBehaviour {
+    private AudioSource audioSrc;
+    private float musicVolume = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        audioSrc = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        audioSrc.volume = musicVolume;
+	}
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+}
---
+++ Assets/Systems/AccessibilitySystem.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using FYFY;
+using UnityEngine.UI;
+using TMPro;
+
+public class AccessibilitySystem : FSystem {
+
+    //creation de famille qui recupere tous les components type Accessibility_settings
+    private Family accessibilitySettings_f = FamilyManager.getFamily(new AllOfComponents(typeof(Accessibility_settings)));
+
+    //creation de famille qui recupere tous les components type Text; TextMeshPro et TextMeshProUGUI
+    private Family text_f = FamilyManager.getFamily(new AnyOfComponents(typeof(Text), typeof(TextMeshPro), typeof(TextMeshProUGUI)));
+
+    private Accessibility_settings accessSettings;
+
+    public AccessibilitySystem ()
+    {
+        if (Application.isPlaying)
+        {
+            //recuparation du tout premier component type Accessibility_settings
+            accessSettings = accessibilitySettings_f.First().GetComponent<Accessibility_settings>();
+            if (accessSettings.enableFont) //Si enableFont de Accessibility_Settings est selectionnee
+            {
+                foreach (GameObject go in text_f) //parcours de tous les GO de la famille text_f
+                {
+                    Text txt = go.GetComponent<Text>();
+                    if (txt != null)
+                    {
+                        txt.font = accessSettings.accessibleFont; //attribution de la police defini dans Accessibility_settings à txt
+                        //GameObjectManager.addComponent<Contour>(go, new { m_size = 1f}); Pour parametrer directement la taille par défaut
+                        GameObjectManager.addComponent<Contour>(go);
+                    }
+                    TextMeshPro tm = go.GetComponent<TextMeshPro>();
+                    if (tm != null)
+                        tm.font = accessSettings.accessibleFontTMPro;
+                    TextMeshProUGUI tmGUI = go.GetComponent<TextMeshProUGUI>();
+                    if (tmGUI != null)
+                        tmGUI.font = accessSettings.accessibleFontTMPro;
+
+
+
+                }
+            }
+        }
+    }
+
+	// Use to process your families.
+	protected override void onProcess(int familiesUpdateCount) {
+	}
+}
