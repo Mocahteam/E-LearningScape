ccde5f3b2089ee47b8c1e7833ec46409e4e745b7
garbarini <delia.garbari@gmail.com>
2019-03-26 11:20:13 +0100

    Add sound effect on gameObject

---
+++ Assets/Scripts/SfxTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SfxTest : MonoBehaviour {
+    public AudioSource fragmentReve;
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Clicked on the dream fragment");
+        fragmentReve.Play();
+    }
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("Passage Souris on dream fragment");
+        fragmentReve.Play();
+    }
+    
+}
