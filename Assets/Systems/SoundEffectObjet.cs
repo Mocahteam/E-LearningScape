5803854792ced3c40e3cb670b2232d0285e32042
garbarini <delia.garbari@gmail.com>
2019-03-28 10:40:12 +0100

    create system to add sound effect when object is hightlight

---
+++ Assets/Systems/SoundEffectObjet.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using FYFY;
+
+public class SoundEffectObjet : FSystem {
+    private Family f_soundObj = FamilyManager.getFamily(new AllOfComponents(typeof(AudioBank), typeof(AudioSource)));
+    private Family f_lightIndiceObjet = FamilyManager.getFamily(new AllOfComponents(typeof(Highlighted)));
+
+    public SoundEffectObjet()
+    {
+        if (Application.isPlaying)
+        {
+            f_lightIndiceObjet.addEntryCallback(onNeedHighlighted);
+        }
+    }
+
+    public void onNeedHighlighted(GameObject go)
+    {
+        Highlighted light = go.GetComponent<Highlighted>();
+        if (go.GetComponent<Highlighted>())
+        {
+            foreach (GameObject lightClues in f_lightIndiceObjet)
+            {
+                f_soundObj.First().GetComponent<AudioSource>().PlayOneShot(f_soundObj.First().GetComponent<AudioBank>().audioBank[8]);
+            }
+        }
+        GameObjectManager.removeComponent<Highlighted>(go);
+    }
+
+	// Use to process your families.
+	protected override void onProcess(int familiesUpdateCount) {
+	}
+}
