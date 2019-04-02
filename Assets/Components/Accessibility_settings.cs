29a982567437e43db0f79900ee83763753750763
garbarini <delia.garbari@gmail.com>
2019-04-02 14:49:48 +0200

    Try replace accessibility button font option by TextMeshPro dropdown and font list

--- Assets/Components/Accessibility_settings.cs
+++ Assets/Components/Accessibility_settings.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using FYFY;
+using UnityEngine.UI;
 
 public class Accessibility_settings : MonoBehaviour {
 
     public bool enableFont = false; // bouton que l'utilisateur coche ou non pour avoir les parametre defini ici
-    public Font accessibleFont; //dans unity et accessibleFont est glisee la police souhaitee
-    public TMP_FontAsset accessibleFontTMPro; //dans unity et accessibleFontTMPro est glisee la police souhaitee
-    public Font defaultFont; //dans unity et accessibleFont est glisee la police souhaitee
-    public TMP_FontAsset defaultFontTMPro; //dans unity et accessibleFontTMPro est glisee la police souhaitee
-    
+    public Font accessibleFont; //dans unity et accessibleFont est glisee la police souhaitee. La police ici est la police dys Accessible dfa
+    public TMP_FontAsset accessibleFontTMPro; //dans unity et accessibleFontTMPro est glisee la police souhaitee. Police dys Accessible dfa
+    public Font defaultFont; //dans unity et accessibleFont est glisee la police souhaitee. Police du jeu Flabby
+    public TMP_FontAsset defaultFontTMPro; //dans unity et accessibleFontTMPro est glisee la police souhaitee. Flabby 
+    //public Font standarFont; //Arial
+    //public TMP_FontAsset standartFontTMPro; // Arial
 
+    /*public Dropdown dropdown;
+    public TextMeshProUGUI policeName;
+    
+    List<string> m_PoliceOptions = new List<string> { "Flabby Bums handwriting", "Dys Accessible DfA", "LibaerationSANS" };
+        
+    public void Dropdown_Index(int index)
+    {
+        policeName.text = m_PoliceOptions[index];
+    }
+
+    public void policeList()
+    {
+        dropdown.AddOptions(m_PoliceOptions);
+    }*/
+   
+    
     public void toggleAccessibleSettings(bool newState)
     {
         enableFont = newState;
