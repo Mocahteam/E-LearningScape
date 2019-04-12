cca4c9cb06489d3898af5e7363270ab00d8b31af
garbarini <delia.garbari@gmail.com>
2019-04-12 18:05:46 +0200

    Add min and max value at zoom and adapt mouseLook

--- Assets/Scripts/ZoomCamera.cs
+++ Assets/Scripts/ZoomCamera.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class ZoomCamera : MonoBehaviour {
     private Camera viewCam;
+    public float minZoom;
+    public float maxZoom;
     
 
     private void Start()
@@ -14,16 +17,31 @@ public class ZoomCamera : MonoBehaviour {
     private void Update()
     {
         //Zoom avant de 10 en 10
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.PageUp) && viewCam.fieldOfView > minZoom)
+        {
             viewCam.fieldOfView -= 10;
+            this.transform.parent.GetComponent<FirstPersonController>().m_MouseLook.XSensitivity -= 0.5f;
+            this.transform.parent.GetComponent<FirstPersonController>().m_MouseLook.YSensitivity -= 0.5f;
+        }
+            
 
         //Zoom arrière de 10 en 10
-        if (Input.GetKeyDown(KeyCode.B))
+        if (Input.GetKeyDown(KeyCode.PageDown) && viewCam.fieldOfView < maxZoom)
+        {
             viewCam.fieldOfView += 10;
+            this.transform.parent.GetComponent<FirstPersonController>().m_MouseLook.XSensitivity += 0.5f;
+            this.transform.parent.GetComponent<FirstPersonController>().m_MouseLook.YSensitivity += 0.5f;
+        }
+           
 
         //Zoom par défaut
-        if (Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.Home))
+        {
             viewCam.fieldOfView = 60;
+            this.transform.parent.GetComponent<FirstPersonController>().m_MouseLook.XSensitivity = 2.5f;
+            this.transform.parent.GetComponent<FirstPersonController>().m_MouseLook.YSensitivity = 2.5f;
+        }
+            
     }
     
 }
