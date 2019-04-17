f7ff2164fe1df76db1f4bb40409d465e95e170e6
garbarini <delia.garbari@gmail.com>
2019-04-17 10:12:20 +0200

    Add comment and rename a variable defaultSize in defaultMaxSize

--- Assets/Components/MaxFontSize.cs
+++ Assets/Components/MaxFontSize.cs
@@ -2,7 +2,7 @@
 
 public class MaxFontSize : MonoBehaviour {
     // Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
-    public int maxSize;
+    public int maxSize; //Valeur renseignée dans le max size jusqu'où le texte peut grossir au max
     [HideInInspector]
-    public float defaultSize;
+    public float defaultMaxSize; //Valeur renseignée dans le max size qui a permis de définir la taille de la police de départ
 }
