037f54e2869e3e84076c6917061cc81fed177bf3
garbarini <delia.garbari@gmail.com>
2019-04-19 15:59:23 +0200

    Create method to modify intensity Light

---
+++ Assets/Components/IntensityLight.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class IntensityLight : MonoBehaviour {
+    // Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
+    public Light sunlight;
+
+    public void setIntensity (float newValue)
+    {
+        sunlight.intensity = newValue;
+    }
+
+}
