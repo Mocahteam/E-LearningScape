67d1d4bdfd5d3ba7fe84c80ccd8317671d354f84
garbarini <delia.garbari@gmail.com>
2019-04-19 11:57:37 +0200

    Initiate outline width and face dilate text mesh pro properties at 0

--- Assets/Components/MaxFontSize.cs
+++ Assets/Components/MaxFontSize.cs
@@ -1,8 +1,18 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class MaxFontSize : MonoBehaviour {
     // Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
     public int maxSize; //Valeur renseignée dans le max size jusqu'où le texte peut grossir au max
     [HideInInspector]
     public float defaultMaxSize; //Valeur renseignée dans le max size qui a permis de définir la taille de la police de départ
+
+    public TMP_Text message;
+
+    public void Start()
+    {
+        message.outlineWidth = 0;
+        message.fontSharedMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, 0);
+    }
+    
 }
