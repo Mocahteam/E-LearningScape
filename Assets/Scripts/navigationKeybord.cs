aa049adc6e1f3d7373144185813f5e031b328444
garbarini <delia.garbari@gmail.com>
2019-04-24 17:36:35 +0200

    Create script to navigate in menu with keyboard

---
+++ Assets/Scripts/navigationKeybord.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class navigationKeybord : MonoBehaviour {
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+    private bool buttonSelected;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxisRaw ("Vertical") != 0 && buttonSelected == false)
+        {
+            eventSystem.SetSelectedGameObject(selectedObject);
+            buttonSelected = true;
+        }
+
+	}
+
+    private void OnDisable()
+    {
+        buttonSelected = false;
+    }
+}
