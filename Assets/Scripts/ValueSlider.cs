1bc2959e4a2c0ab4f40d455652334a51eb1c748e
garbarini <delia.garbari@gmail.com>
2019-05-10 14:25:15 +0200

    Add dynamic text value on slider

--- Assets/Scripts/ValueSlider.cs
+++ Assets/Scripts/ValueSlider.cs
@@ -11,12 +11,19 @@ public class ValueSlider : MonoBehaviour {
 	void Start () {
 		percentageTextSlider = GetComponent<TMP_Text> ();
 	}
-	
+
+	//if value is on 0 and 1
 	public void textUpdatePercent (float valueSlider){
 		percentageTextSlider.text = Mathf.RoundToInt (valueSlider * 100) + "%";
 	}
 
+	//if value is on 0 and 100
 	public void textUpdate (float valueSlider){
 		percentageTextSlider.text = Mathf.RoundToInt (valueSlider) + "%";
+	}
+
+	//if value is on 0 and 10
+	public void textUpdateTenPercent (float valueSlider){
+		percentageTextSlider.text = Mathf.RoundToInt (valueSlider * 10) + "%";
 	}
 }
