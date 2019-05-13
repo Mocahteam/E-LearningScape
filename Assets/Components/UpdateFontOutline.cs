6561fcf7fa0bd07e402b0f4c5ad8673cd66f3cea
garbarini <delia.garbari@gmail.com>
2019-05-13 16:30:03 +0200

    Delete component associated at function to modify outline width

--- Assets/Components/UpdateFontOutline.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-public class UpdateFontOutline : MonoBehaviour {
-    // Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
-    public float newWidthContour;
-}
