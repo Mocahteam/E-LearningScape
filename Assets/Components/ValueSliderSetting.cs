fb638e705e7d2fac71553373755368c7dca5986c
garbarini <delia.garbari@gmail.com>
2019-05-29 16:52:56 +0200

    Add new component called in accessibility_system and accessibility_settings Create and add new animation in animator function to HUD_H

---
+++ Assets/Components/ValueSliderSetting.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class ValueSliderSetting : MonoBehaviour {
+    // Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
+    //[HideInInspector]
+    public float defaultValueSlider;
+}
