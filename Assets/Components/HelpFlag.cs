6cbd5906875fc68e801b498859c812945455c791
Muratet <mathieu.muratet@gmail.com>
2019-10-10 18:27:36 +0200

    Add missing files

---
+++ Assets/Components/HelpFlag.cs
@@ -0,0 +1,5 @@
+﻿using UnityEngine;
+
+public class HelpFlag : MonoBehaviour {
+    // Just a flag component
+}
---
+++ Assets/Components/RemoveIfVeryVeryLow.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveIfVeryVeryLow : MonoBehaviour {
+    // Just a flag component
+}
