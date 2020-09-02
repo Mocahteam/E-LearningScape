0d2c64c91fcad55c3510e540f6d5ae33736f064f
Muratet <mathieu.muratet@gmail.com>
2020-09-02 10:56:16 +0200

    Remove PetriNet descriptions

--- Assets/Systems/StoryDisplaying.cs
+++ Assets/Systems/StoryDisplaying.cs
@@ -184,7 +184,6 @@ public class StoryDisplaying : FSystem {
                         CollectObject.instance.Pause = false;
                         MoveInFrontOf.instance.Pause = false;
                         UIEffectPlayer.instance.Pause = false;
-                        ActionsManager.instance.Pause = !LoadGameContent.gameContent.trace;
                     }
                     else
                         GameObjectManager.loadScene(SceneManager.GetActiveScene().name); // reset game
