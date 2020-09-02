1897ca4528880626ece81896ffa093bbe55a1540
Muratet <mathieu.muratet@gmail.com>
2020-09-02 10:59:42 +0200

    Remove IARPuzzleManager

--- Assets/Components/puzzleDeltaPositions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class puzzleDeltaPositions : MonoBehaviour {
-    // Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
-    public List<float> xDelta;
-    public List<float> yDelta;
-}
