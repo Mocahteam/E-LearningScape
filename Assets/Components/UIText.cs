1c1ba433682910ba979bbd64d3ee1c6ef369e5d5
DarkMoun <elbaf828@gmail.com>
2020-11-12 18:32:43 +0100

    Extraction des textes des UI dans les fichiers json (files were missing)

---
+++ Assets/Components/UIText.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class UIText : MonoBehaviour {
+	// Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
+
+	//the name of the variable in GameContent corersponding to this UI text
+	public string gameContentVariable;
+
+	//this bool is false if the text is in a Text Mesh Pro component, and true if it is in a Text Mesh Pro UGUI component
+	public bool tmpUI = true;
+}
