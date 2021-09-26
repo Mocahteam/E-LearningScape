612824fa4e0b27f8efa9bed592187d21732e76b7
Muratet <mathieu.muratet@gmail.com>
2021-09-26 22:52:29 +0200

    Add missing files

---
+++ Assets/Scripts/GameSelectionWrapper.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GameSelectionWrapper : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private GameSelected gameSelected;
+
+    void Start()
+    {
+        GameObject go = GameObject.Find("GameSelected");
+        if (go)
+            gameSelected = go.GetComponent<GameSelected>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (gameSelected)
+            gameSelected.setDetails(gameObject.name);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (gameSelected)
+            gameSelected.removeDetails();
+    }
+
+    public void startGame()
+    {
+        if (gameSelected)
+            gameSelected.startGame(gameObject.name);
+    }
+}
